7aa79391d6470ffc704ff869c62ab183ea42047e
root <root@debianWheezy>
2014-10-20 09:32:21 -0300

    Se agrego la base del sistema.

---
+++ fuentes/application/views/Formularios/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Formulario Din&aacute;mico</title>
+    
+    <!-- Cabecera -->
+    <?php $this->load->view('comunes/cabecera')?>
+</head>
+<body>
+    <div class="container cabecera">
+        <h1>Formulario Din&aacute;mico</h1>
+    </div>
+    
+    
+    hola mundo
+    
+    
+    <!-- Pie de pagina -->
+    <?php $this->load->view('comunes/pie_pagina')?>
+</body>
+</html>
---
+++ fuentes/application/views/comunes/cabecera.php
@@ -0,0 +1,16 @@
+<!-- Inicio de cabecera -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="<?php echo base_url(); ?>js/libs/FormBuilder/formbuilder-min.css" />
+
+<script src="<?php echo base_url(); ?>js/libs/FormBuilder/formbuilder-min.js" type="text/javascript" charset="utf-8"></script>
+
+
+<!-- Versión compilada y comprimida del JavaScript de Bootstrap -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+
+<!-- Fin de cabecera -->
