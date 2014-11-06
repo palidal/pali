d863c431e718c6492b1bb8f6fe922b6d37ba2467
josego85@gmail.com <josego85@gmail.com>
2014-11-06 16:20:34 -0300

    Se esta probando el tamanhio de la imagen en el login

--- fuentes/application/views/Login/alumnos.php
+++ fuentes/application/views/Login/alumnos.php
@@ -11,17 +11,12 @@
 	</style>
 </head>
 <body>
-	<img src="<?php echo base_url(); ?>recursos/imagenes/logo/pali.jpg" alt="Pali" title="Pali" width="30%" height="50%" class="img-responsive center-block">
+	<img src="<?php echo base_url(); ?>recursos/imagenes/logo/pali.jpg" alt="Pali" title="Pali" width="200px" height="300px" class="img-responsive center-block">
     <div class="container">
         <form class="form-signin" role="form" action="/Login/login" method="post">
             <h2 class="form-signin-heading">Por favor, inicie sesión</h2>
             <input type="text" name="usuario" class="form-control" placeholder="Usuario" required autofocus>
             <input type="password" name="password"class="form-control" placeholder="Contraseña" required>
-<!--         <div class="checkbox"> -->
-<!--             <label> -->
-<!--                 <input type="checkbox" value="remember-me"> Remember me -->
-<!--             </label> -->
-<!--         </div> -->
             <button class="btn btn-lg btn-primary btn-block" type="submit">Acceder</button>
         </form>
     </div> <!-- /container -->
